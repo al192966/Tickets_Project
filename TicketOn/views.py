2524c5b097f458c7ec6a4e8c410e4a3391932238
Nathalie <al192966@alumnos.uacj.mx>
2023-10-26 18:14:35 -0600

    Diseño de home

--- TicketOn/views.py
+++ TicketOn/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,6 +10,17 @@ def Home(request):
 
 def signup(request):
     
-    return render(request, 'login.html',{
-        'form': UserCreationForm
-    })
+    if request.method =='GET':
+        return render(request, 'login.html',{
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registro de usuario
+            try:
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usario Creado Correctamente')
+            except:
+                return HttpResponse('Usuario ya existe')
+        return HttpResponse ('Las contraseñas no coinciden'),
